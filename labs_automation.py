5458757429b3a20e060684f6f2e23dde9a0482d7
sadhanareddy <sadhana.virupaksha@gmail.com>
2016-09-20 18:54:11 +0530

    added sources

--- labs_automation.py
+++ labs_automation.py
@@ -82,15 +82,38 @@ def fetch_expt_sections_links(url):
 	return list_without_http
 	        
 def fetch_exptspage_content(explinks):
-	dir_name =  "expt" + str(sectionlink)
-	os.mkdir(dir_name)
+	# dir_name =  "expt" + str(sectionlink)
+	# os.mkdir(dir_name)
 	for section_link in range(len(explinks)):
 		html = requests.get(explinks[section_link])
-		soup = BeautifulSoup(html.content)                                                                                                      
+		soup = BeautifulSoup(html.content)
+		# soup.find_all('img').get('src') = urlparse.urljoin(explinks[section_link], image_src)
+		for img_tag in soup.find_all('img'):
+			image_src = str(img_tag.get('src'))
+		
+			tag = urlparse.urljoin(explinks[section_link], image_src)
+			print tag
+		# 	soup.find('img')['src'] = tag
+		# 	print soup.find('img')['src']
+		# 	print soup
+		# 	image_src.replace_with(tag)
+			# print soup
+			# markup = '<a href="http://example.com/">I linked to <i>example.com</i></a>'
+			# soup = BeautifulSoup(markup)
+			# a_tag = soup.find_all('img')
+
+			# new_tag = soup.new_tag("b")
+			# new_tag.string = "example.net"
+			# image_src.replace_with(tag)
+			# print image_src.replace_with(tag)
+		soup = BeautifulSoup(html.content)	
 		sectioncontent = str(soup.h1.next_sibling)
 		labspec = json.loads(open('labspec.json', 'r').read())
-		filename = labspec['experiments'][section_link + 1]['subsections'][section_link]['name'] + '.html'
-		file = open(os.path.join(dir_name, filename), 'w')
+		filename1 = labspec['experiments'][i]['name']+"_"+labspec['experiments'][section_link + 1]['subsections'][section_link]['name']+ "_"+"Unit"+"_"+"html"+'.html'
+		filename = filename1.replace(" ","_")
+		# filename = labspec['experiments'][section_link + 1]['subsections'][section_link]['name'] + '.html'
+		# file = open(os.path.join(dir_name, filename), 'w')
+		file = open(filename, 'w')
 		try:
 			file.write(sectioncontent)
 			file.close()
@@ -99,18 +122,19 @@ def fetch_exptspage_content(explinks):
 			print str(e)
 
 
-lab_url = "http://cse14-iiith.vlabs.ac.in/"
+lab_url = "http://sd-iiith.vlabs.ac.in/"
 final_list = fetch_urls_of_homepage(lab_url) 
 # print final_list
 
 fetch_homepage_content(final_list)
 
 
-expt_url = "http://cse14-iiith.vlabs.ac.in/List%20of%20experiments.html?domain=Computer%20Science"
+expt_url = "http://sd-iiith.vlabs.ac.in/List%20of%20experiments.html?domain=Civil%20Engineering"
 experiment_links = fetch_urls_of_exptpage(expt_url)
 # print experiment_links
-
+i = 0
 for sectionlink in range(len(experiment_links)):
+	i = i+1
 	explinks = fetch_expt_sections_links( experiment_links[sectionlink])
 	# print explinks
 	fetch_exptspage_content(explinks)
